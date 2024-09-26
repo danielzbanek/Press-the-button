a389c8948d5877613b2dc19df09695f09e6f5051
danielzbanek <zbanek.daniel@ssakhk.cz>
2024-09-26 21:02:46 +0200

    Add project files.

---
+++ Press the button/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Press_the_button
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Title = new Label();
+            button1 = new Button();
+            Stage = new ProgressBar();
+            StageLabel = new Label();
+            label1 = new Label();
+            RestartButton = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // Title
+            // 
+            Title.AutoSize = true;
+            Title.BackColor = Color.FromArgb(192, 255, 255);
+            Title.BorderStyle = BorderStyle.Fixed3D;
+            Title.FlatStyle = FlatStyle.System;
+            Title.Font = new Font("Stencil", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Title.Location = new Point(1, -2);
+            Title.Name = "Title";
+            Title.Size = new Size(477, 226);
+            Title.TabIndex = 0;
+            Title.Text = "Press The Button \r\n\r\nTHE GAME\r\n";
+            Title.TextAlign = ContentAlignment.BottomLeft;
+            Title.Click += Title_Click;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.BackColor = Color.FromArgb(0, 192, 192);
+            button1.Location = new Point(809, 222);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 250);
+            button1.TabIndex = 1;
+            button1.Text = "Button";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Stage
+            // 
+            Stage.Anchor = AnchorStyles.None;
+            Stage.Location = new Point(579, 361);
+            Stage.Name = "Stage";
+            Stage.Size = new Size(214, 29);
+            Stage.TabIndex = 2;
+            // 
+            // StageLabel
+            // 
+            StageLabel.Anchor = AnchorStyles.None;
+            StageLabel.AutoSize = true;
+            StageLabel.ForeColor = SystemColors.ActiveCaptionText;
+            StageLabel.Location = new Point(579, 393);
+            StageLabel.Name = "StageLabel";
+            StageLabel.Size = new Size(17, 20);
+            StageLabel.TabIndex = 3;
+            StageLabel.Text = "0";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            label1.Location = new Point(636, 393);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 20);
+            label1.TabIndex = 4;
+            label1.Text = " ↑ Your Stage↑ \r\n";
+            // 
+            // RestartButton
+            // 
+            RestartButton.Anchor = AnchorStyles.None;
+            RestartButton.Location = new Point(1028, -2);
+            RestartButton.Name = "RestartButton";
+            RestartButton.Size = new Size(79, 79);
+            RestartButton.TabIndex = 5;
+            RestartButton.Text = "restart\r\n\r\n";
+            RestartButton.UseVisualStyleBackColor = true;
+            RestartButton.Click += RestartButton_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(169, 340);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 6;
+            label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 683);
+            Controls.Add(label2);
+            Controls.Add(RestartButton);
+            Controls.Add(label1);
+            Controls.Add(StageLabel);
+            Controls.Add(Stage);
+            Controls.Add(button1);
+            Controls.Add(Title);
+            Name = "Form1";
+            Text = "Press the Button";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Title;
+        private Button button1;
+        private ProgressBar Stage;
+        private Label StageLabel;
+        private Label label1;
+        private Button RestartButton;
+        private Label label2;
+    }
+}
---
+++ Press the button/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace Press_the_button
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        int cycle1 = 0;
+        int cycle2 = 0;
+        int StageLable = 0;
+        int cycle3 = 0;
+        public bool Zamceno = false;
+
+        public Form1()
+        {
+            Random random = new Random();
+
+            InitializeComponent();
+            if (Zamceno == true)
+            {
+               button1.ForeColor = Color.Black;
+            }
+        }
+        
+        private void Title_Click(object sender, EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (Stage.Value == Stage.Maximum)
+            {
+                MessageBox.Show("vyhral si, gratuluji");
+                return;
+            }
+            switch (i)
+            {
+                case 0:
+                    stage1();
+                    break;
+                case 1:
+                    stage2();
+                    break;
+                case 2:
+                    stage3();
+                    break;
+                case 3:
+                    stage4();
+                    break;
+
+
+            }
+
+            int stage1()
+            {
+                if (cycle1 >= 2)
+                {
+                    i++;
+                }
+                if (cycle1 == 0 || cycle1 == 1)
+                {
+
+
+                    if (Stage.Value == 9)
+                    {
+                        if (cycle1 == 0)
+                        {
+                            MessageBox.Show("zmáčkl jsi ho 10krát, necitil jsem skvěle? co takhle znova");
+
+                        }
+                        if (cycle1 == 1)
+                        {
+                            MessageBox.Show("dobra prace, zmáčkl jsi to znova 10krát");
+                        }
+
+                        Stage.Value = 0;
+                        StageLabel.Text = Stage.Value.ToString() + "/100";
+                        cycle1++;
+                    }
+                    else
+                    {
+                        Stage.Value++;
+                        StageLabel.Text = Stage.Value.ToString() + "/100";
+                    }
+                }
+                return i;
+            }
+            int stage2()
+            {
+                if (cycle2 == 0)
+                {
+                    MessageBox.Show("Daří se ti skvělě,přičtu ti 10");
+                    Stage.Value = 10;
+                    cycle2++;
+                }
+                if (Stage.Value == 38 && cycle2 == 1)
+                {
+                    i++;
+                }
+                Stage.Value++;
+                Stage.Maximum = 100;
+                StageLabel.Text = Stage.Value.ToString() + "/100";
+
+                return i;
+            }
+            int stage3()
+            {
+                if (Stage.Value == 99 && cycle3 == 60)
+                {
+                    MessageBox.Show("šlo ti to skvěle,tak si to zopakujeme,ale tentokrát to bude těžší");
+
+                    Stage.Value = 0;
+                    StageLabel.Text = Stage.Value.ToString() + "/100";
+                    i++;
+                }
+                cycle3++;
+                Stage.Value++;
+                StageLabel.Text = Stage.Value.ToString() + "/100";
+                MessageBox.Show("nespamuj ");
+                label2.Text = cycle3.ToString();
+                return i;
+
+
+            }
+            int stage4()
+            {
+                Zamceno = true;
+              
+                //int formWidth = this.ClientSize.Width;
+                //int formHeight = this.ClientSize.Height;
+
+
+                //int buttonWidth = Button.Width;
+                //int buttonHeight = Button.Height;
+
+
+                ////int x = random.Next(0, formWidth - buttonWidth);
+                ////int y = random.Next(0, formHeight - buttonHeight);
+
+
+                ////btnRandomPosition.Location = new Point(x, y);
+                return i;
+            }
+            int stage5()// schovane zamcene tlacitko
+            {
+                return i;
+            }
+            int stage6()//  vložte file (přes FileSystémWatcher)
+            {
+                return i;
+            }
+            int stage7()// pes vám jí stage! musíte mačkat rychleji (timer)
+            {
+                    return i;   
+            }       
+            int stage8()//  Vložte obrázek (maybye)
+            {
+                return i;
+            }
+        }
+
+        private void RestartButton_Click_1(object sender, EventArgs e)
+        {
+            Stage.Value = 0;
+            cycle1 = 0;
+            i = 0;
+            StageLabel.Text = Stage.Value.ToString() + "/100";
+            cycle2 = 0;
+            cycle3 = 0;
+            label2.Text = cycle3.ToString();
+        }
+    }
+}
+
+
+//libor was here ;) :* :P
